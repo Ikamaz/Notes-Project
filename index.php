7b20d57f4985810a59f774bc62b8e39adf45d93b
Irakli Mazmishvili <mazmishviliika@gmail.com>
2024-02-19 04:26:41 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'functions.php';
+require "Database.php";
+
+$config = require('config.php');
+
+$db = new Database($config['database']);
+$posts = $db -> query('select * from posts ')-> fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
---
+++ views/partials/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+<head>
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+    <div class="min-h-full">
